4a80b42f658fc397edafefbee05fa147ecc9127b
Tuned Chaos LLC <lane@tunedchaos.com>
2019-08-14 17:57:06 -0400

    Fixed an issue where the devtools were enabled on a release

--- src/index.js
+++ src/index.js
@@ -192,7 +192,6 @@ function createWindows () {
     aboutWindow.webContents.openDevTools()
     preferenceWindow.webContents.openDevTools()
   }else{
-    mainWindow.webContents.openDevTools()
     childWindow = new BrowserWindow({
       title: 'SMM2_StatusBar',
       width: 800,
